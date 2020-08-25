834c38ba9059dfc29088b8891bf505a18235053f
Stan Verschuuren <sverschu@student.codam.nl>
2020-08-25 19:27:53 +0200

    added internal header

---
+++ inc/execblocks_internal.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   execblocks_internal.h                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sverschu <sverschu@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/25 18:17:02 by sverschu      #+#    #+#                 */
+/*   Updated: 2020/08/25 19:27:20 by sverschu      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECBLOCKS_INTERNAL_H
+# define EXECBLOCKS_INTERNAL_H
+
+# include "execblocks.h"
+
+typedef struct		t_node
+{
+	
+}					s_node;
+
+#endif
