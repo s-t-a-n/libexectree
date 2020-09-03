a0b3b4283d25bd1c5b821c907e5d3822889d72fe
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-03 15:25:24 +0200

    fixed memory leak

--- src/tokentable_lifetime.c
+++ src/tokentable_lifetime.c
@@ -6,7 +6,7 @@
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/01 18:40:13 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/09/02 20:14:19 by sverschu      ########   odam.nl         */
+/*   Updated: 2020/09/03 15:25:00 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ t_tokentable		*tokentable_destroy(t_tokentable *tokentable)
 		}
 		tokentable->size--;
 	}
+	free(tokentable->tokens);
 	free(tokentable);
 	return (NULL);
 }
