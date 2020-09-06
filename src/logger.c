72cdc4f24b9afa5d805d7b3e59e06c973fe8f3c4
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-06 18:01:06 +0200

    improved logger to decrease write calls

--- src/logger.c
+++ src/logger.c
@@ -6,7 +6,7 @@
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/05 22:34:25 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/09/05 22:38:55 by sverschu      ########   odam.nl         */
+/*   Updated: 2020/09/06 15:20:51 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,50 @@
 #include "libft.h"
 #include "logging.h"
 
+/*
+** Excuse the overhead when DEBUG is unset; 42 doesn't allow macro's..
+*/
+
+#ifdef DEBUG
+
 void	logger(	t_error errl,
 				const char *header,
 				const char *body,
 				const char *tail)
 {
+	char	buffer[1024];
+	
+	buffer[0] = '\0';
 	if (errl == CRIT)
-		write(2, "\x1b[31;01m", 10);
+		ft_strlcat(buffer, "\x1b[31;01m", LOG_BUF_SIZE);
 	else if (errl == WARN)
-		write(2, "\x1b[33;01m", 10);
+		ft_strlcat(buffer, "\x1b[33;01m", LOG_BUF_SIZE);
 	else
-		write(2, "\x1b[32;01m", 10);
-	write(2, header, ft_strlen(header));
-	write(2, "\x1b[0m", 6);
-	write(2, " -> ", 4);
-	write(2, body, ft_strlen(body));
-	write(2, " :` ", 3);
-	write(2, tail, tail ? ft_strlen(tail) : 0);
+		ft_strlcat(buffer, "\x1b[32;01m", LOG_BUF_SIZE);
+	ft_strlcat(buffer, header, LOG_BUF_SIZE);
+	ft_strlcat(buffer, "\x1b[0m", LOG_BUF_SIZE);
+	ft_strlcat(buffer, " -> ", LOG_BUF_SIZE);
+	ft_strlcat(buffer, body, LOG_BUF_SIZE);
+	if (tail)
+	{
+		ft_strlcat(buffer, " : ", LOG_BUF_SIZE);
+		ft_strlcat(buffer, tail, LOG_BUF_SIZE);
+	}
+	ft_strlcat(buffer, "\n", LOG_BUF_SIZE);
+	write(errl == INFO ? STD_OUT : STD_ERR, buffer, ft_strlen(buffer));
 }
+
+#else
+
+void	logger(	t_error errl,
+				const char *header,
+				const char *body,
+				const char *tail)
+{
+	(void)errl;
+	(void)header;
+	(void)body;
+	(void)tail;
+}
+
+#endif
