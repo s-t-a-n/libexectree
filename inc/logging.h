02862ddc895334c7d4688920e5a687e5cb6abcbc
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-06 18:02:06 +0200

    added definitions for initialisation

--- inc/logging.h
+++ inc/logging.h
@@ -6,12 +6,17 @@
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/05 22:35:29 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/09/05 22:38:12 by sverschu      ########   odam.nl         */
+/*   Updated: 2020/09/06 15:14:25 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LOGGING_H
 # define LOGGING_H
+
+# define LOG_BUF_SIZE 1024
+
+# define STD_OUT			1
+# define STD_ERR			2
 
 typedef enum		e_error
 {
