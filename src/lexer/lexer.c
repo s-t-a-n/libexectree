7fa16994b4a378b562ba000fb718854e28af2eeb
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-20 19:03:38 +0200

    minor bump pre branching

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,8 +6,9 @@
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/20 18:31:36 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/09/20 18:31:47 by sverschu      ########   odam.nl         */
+/*   Updated: 2020/09/20 19:02:17 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+
