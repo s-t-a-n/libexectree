cce996ee9ff5c1c4642b10bc5bdef3b9251f0fc8
Stan Verschuuren <sverschu@student.codam.nl>
2020-09-20 19:50:24 +0200

    overhaul; thrown away most old global files to reboot general architecture 2

--- inc/exectree.h
+++ inc/exectree.h
@@ -6,7 +6,7 @@
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/28 19:37:59 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/09/20 19:06:54 by sverschu      ########   odam.nl         */
+/*   Updated: 2020/09/20 19:50:08 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,5 @@
 # ifdef DEBUG
 #  include <stdio.h>
 # endif
-
-void				*exectree_create();
-void				*exectree_destroy(void *_tree);
-
-void				*exectree_populate(void *_tree, t_token *_token);
-void				*exectree_build(void *_tree, const char *str);
-void				*exectree_execute(void *_tree);
 
 #endif
