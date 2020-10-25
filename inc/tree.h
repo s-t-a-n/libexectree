3a6d3fd83bb9cf6d8cafe203187253ed1847a26c
Stan Verschuuren <sverschu@student.codam.nl>
2020-10-25 20:32:55 +0100

    changed node.h -> tree.h for clarity of scope

--- inc/tree.h
+++ inc/tree.h
@@ -1,31 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   exectree_internal.h                                :+:    :+:            */
+/*   tree.h                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sverschu <sverschu@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2020/08/28 19:38:12 by sverschu      #+#    #+#                 */
-/*   Updated: 2020/10/25 20:31:54 by sverschu      ########   odam.nl         */
+/*   Created: 2020/10/25 20:31:26 by sverschu      #+#    #+#                 */
+/*   Updated: 2020/10/25 20:31:36 by sverschu      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXECTREE_INTERNAL_H
-# define EXECTREE_INTERNAL_H
+#ifndef TREE_H
+# define TREE_H
 
-# ifdef DEBUG
-#  include <stdio.h>
-# endif
-
-# include "exectree.h"
-# include "lexer_ir.h"
-# include "tree.h"
-
-typedef struct	s_exectree
+typedef struct	s_node t_node;
+typedef struct	s_node
 {
-	t_lexer_ir	*lex_ir;
-	t_node		*lex_tree;
-	t_node		*parse_tree;
-}				t_exectree;
+	t_node		*parent;
+	t_node		*left;
+	t_node		*right;
+	void		*children;
+}				t_node;
+
+t_node			*node_create();
+t_node			*node_destroy(t_node *node);
 
 #endif
