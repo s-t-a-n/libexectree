3213eb4c4110d3117d6e0f8054f78034193c02c9
s-t-a-n <stan@wopwop.cc>
2020-11-02 22:37:51 +0100

    intermediary commit ; making proccess_nonterminal and token_lifetime multikey proof

--- src/grammargenerator/gg_process_nonterminal.c
+++ src/grammargenerator/gg_process_nonterminal.c
@@ -16,97 +16,74 @@
 #include "logger.h"
 #include "grammargenerator.h"
 
-static t_gram_token	*gen_token_single(	t_grammar_ir *ir,
-								t_gram_production *production,
-								char *key)
-{
-	t_gram_token				*token;
-
-	if (ft_strcmp(production->nonterminal, key) == 0)
-		token = gramgen_token_create(NONTERMINAL, production);
-	else if ((production = grammar_ir_find_production(ir, key)))
-		token = gramgen_token_create(NONTERMINAL, production);
-	else if ((token = gramgen_token_create(NONTERMINAL, ft_strdup(key))))
-		lst_addback(&ir->post, token);
-	else
-		token = NULL;
-	return (token);
-}
-
-//static t_gram_token	*gen_token_multi(	t_grammar_ir *ir,
-//								t_gram_production *production,
-//								char *key)
-//{
-//	t_gram_token				*token;
-//
-//	if (ft_strcmp(production->nonterminal, key) == 0)
-//		token = gramgen_token_create(NONTERMINAL, production);
-//	else if ((production = grammar_ir_find_production(ir, key)))
-//		token = gramgen_token_create(NONTERMINAL, production);
-//	else if ((token = gramgen_token_create(NONTERMINAL, ft_strdup(key))))
-//		lst_addback(&ir->post, token);
-//	else
-//		token = NULL;
-//	return (token);
-//}
-
 /*
 ** type is already checked before call to gg_process_nonterminal
+** this is highly unoptimized. how to do this in a better way ?
 */
 
-static t_gram_rule_type get_type(char **line, char **d)
+static t_gram_rule_type get_type(char **line)
 {
 	t_gram_rule_type type;
 
 	if (**line == '<')
-	{
-		*d = ">";
-		type = ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strcsetlen(*line, *d)
+		type = ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strclen(*line, **line + 2)
 				? NONTERMINAL_MULT : NONTERMINAL;
-	}
 	else if (**line == '{')
-	{
-		*d = "}";
-		type = ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strcsetlen(*line, *d)
+		type = ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strclen(*line, **line + 2)
 				? UNBOUND_NONTERMINAL_MULT : UNBOUND_NONTERMINAL;
-	}
-	else if (**line == '|')
-	{
-		*d = "|";
+	else if (**line == '[')
 		type = RECURSIVE_NONTERMINAL;
-	}
 	else
 	{
 		logger(WARN, 3, "process_nonterminal, get_type", "couldn't find type for rule", line);
-		*d = NULL;
 		type = UNKNOWN;
 	}
 	(*line)++;
 	return (type);
 }
 
-static uint8_t		add_token_for_key(	t_gram_production *production,
-										t_gram_rule *def,
-										t_grammar_ir *ir,
-										char *key)
+static t_gram_token	*gen_token(t_grammar_ir *ir,
+								t_gram_production *production,
+								t_gram_rule_type type,
+								char *key)
 {
-	t_gram_token	*token;
+	t_gram_token				*token;
 
-	token = gen_token_single(ir, production, key);
-	if (token)
-	{
-		if (vector(&def->tokens, V_PUSHBACK, 0, token))
-		{
-			logger(INFO, production ? 3 : 4,
-						"grammar_generator",
-						"adding nonterminal to rule",
-						key, production ? "" : "adding to Post!");
-			return (0);
-		}
-	}
-	gramgen_token_destroy(token);
-	return (1);
+	if (ft_strcmp(production->nonterminal, key) == 0)
+		token = gramgen_token_create(type, production);
+	else if ((production = grammar_ir_find_production(ir, key)))
+		token = gramgen_token_create(type, production);
+	else if ((token = gramgen_token_create(type, ft_strdup(key))))
+		lst_addback(&ir->post, token);
+	else
+		token = NULL;
+	return (token);
 }
+
+
+// this function is bullshit now
+//static uint8_t		add_token_for_key(	t_gram_production *production,
+//										t_gram_rule *def,
+//										t_grammar_ir *ir,
+//										char *key)
+//{
+//	t_gram_token	*token;
+//
+//	token = gen_token(ir, production, key);
+//	if (token)
+//	{
+//		if (vector(&def->tokens, V_PUSHBACK, 0, token))
+//		{
+//			logger(INFO, production ? 3 : 4,
+//						"grammar_generator",
+//						"adding nonterminal to rule",
+//						key, production ? "" : "adding to Post!");
+//			return (0);
+//		}
+//	}
+//	gramgen_token_destroy(token);
+//	return (1);
+//}
 
 uint8_t				gg_process_nonterminal(t_gram_production *production,
 											t_gram_rule *def,
@@ -115,40 +92,51 @@ uint8_t				gg_process_nonterminal(t_gram_production *production,
 {
 	t_gram_rule_type		type;
 	char					*key;
+	char					closechar;
 	char					*excrement;
-	char					*delimiter;
 
-	type = get_type(line, &delimiter);
-	// loop this using ft_strtok, and add multiple keys to token
-	if (ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strcsetlen(*line, PRODUCTION_CLOSESET))
+	closechar = **line + 2;
+	type = get_type(line);
+	key = ft_strtok(*line, PRODUCTION_DELIMSET);
+	while (key)
 	{
-		// multi key
-		key = ft_strtok(*line, PRODUCTION_DELIMSET);
-		while (key)
-		{
-			if ((excrement = ft_strstr(key, delimiter)))
-				*excrement = '\0';
-			printf("multi-key is : |%s|\n", key);
-
-			// call to gen_token_multi with static var like strtok
-			key = ft_strtok(NULL, PRODUCTION_DELIMSET);
-		}
-	}
-	else
-	{
-		// single key
-		key = ft_strtok(*line, PRODUCTION_DELIMSET);
-		if ((excrement = ft_strstr(key, delimiter)))
+		if ((excrement = ft_strchr(key, closechar)))
 			*excrement = '\0';
-		printf("single-key is : |%s|\n", key);
+		printf("multi-key is : |%s|\n", key);
+		// push key here
+		key = ft_strtok(NULL, PRODUCTION_DELIMSET);
 	}
+
+	//// loop this using ft_strtok, and add multiple keys to token
+	//if (ft_strcsetlen(*line, PRODUCTION_DELIMSET) < ft_strcsetlen(*line, PRODUCTION_CLOSESET))
+	//{
+	//	// multi key
+	//	key = ft_strtok(*line, PRODUCTION_DELIMSET);
+	//	while (key)
+	//	{
+	//		if ((excrement = ft_strchr(key, delim)))
+	//			*excrement = '\0';
+	//		printf("multi-key is : |%s|\n", key);
+
+	//		// call to gen_token_multi with static var like strtok
+	//		key = ft_strtok(NULL, PRODUCTION_DELIMSET);
+	//	}
+	//}
+	//else
+	//{
+	//	// single key
+	//	key = ft_strtok(*line, PRODUCTION_DELIMSET);
+	//	if ((excrement = ft_strchr(key, delim)))
+	//		*excrement = '\0';
+	//	printf("single-key is : |%s|\n", key);
+	//}
 
 	// pivot this to single push of token -> make gen_token_for_key(or keys with array)
-	if (add_token_for_key(production, def, ir, key) == 0)
-	{
-		free(key);
-		return (0);
-	}
+	//if (add_token_for_key(production, def, ir, key) == 0)
+	//{
+	//	free(key);
+	//	return (0);
+	//}
 	free(key);
 	return (1);
 }
--- src/grammargenerator/gg_token_lifetime.c
+++ src/grammargenerator/gg_token_lifetime.c
@@ -13,11 +13,37 @@
 #include "vector.h"
 #include "grammargenerator.h"
 
-t_gram_token	*gramgen_token_create(t_gram_rule_type type, void *sig)
+static t_gram_token		*gramgen_token_create_multikey(	t_gram_rule_type type,
+														void *sig)
 {
-	t_gram_token *token;
+	static t_gram_token	*token;
+	t_gram_token		*subtoken;
 
 	if (sig == NULL)
+	{
+		token = NULL;
+		return (NULL);
+	}
+	if (token || (token = ft_calloc(sizeof(t_gram_token), 1)))
+	{
+		if (!token->sig && !vector(&token->sig, V_CREATE, VEC_DEF_SIZE, NULL))
+			return (NULL);
+		if ((subtoken = gramgen_token_create(type/2, sig)))
+		{
+			if (vector(&token->sig, V_PUSHBACK, 0, subtoken))
+				return (token);
+		}
+	}
+	return (NULL);
+}
+
+t_gram_token		*gramgen_token_create(t_gram_rule_type type, void *sig)
+{
+	t_gram_token	*token;
+
+	if (type & (NONTERMINAL_MULT|UNBOUND_NONTERMINAL_MULT))
+		return (gramgen_token_create_multikey(type, sig));
+	else if (sig == NULL)
 		return (NULL);
 	token = malloc(sizeof(t_gram_token));
 	if (token)
@@ -30,10 +56,21 @@ t_gram_token	*gramgen_token_create(t_gram_rule_type type, void *sig)
 
 t_gram_token		*gramgen_token_destroy(t_gram_token *token)
 {
-	if (token
-		&& (token->type == TERMINAL || token->type == UNBOUND_NONTERMINAL))
+	if (token)
 	{
-		free(token->sig);
+		if (token->sig && token->type & (NONTERMINAL_MULT|UNBOUND_NONTERMINAL_MULT))
+		{
+			while (*(size_t *)vector(&token->sig, V_SIZE, 0, NULL) > 0)
+			{
+				gramgen_token_destroy(vector(&token->sig, V_PEEKBACK, 0, NULL));
+				vector(&token->sig, V_POPBACK, 0, NULL);
+			}
+			vector(&token->sig, V_DESTROY, false, NULL);
+		}
+		else if (token->type & (TERMINAL|UNBOUND_NONTERMINAL))
+		{
+			free(token->sig);
+		}
 	}
 	free(token);
 	return (NULL);
